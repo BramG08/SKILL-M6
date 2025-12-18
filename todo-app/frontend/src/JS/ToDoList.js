a25429e2895453422071cd316e29ef773ab28b63
BramG08 <bgrippeling08@gmail.com>
2025-12-18 12:08:32 +0100

    Updated

--- todo-app/frontend/src/JS/ToDoList.js
+++ todo-app/frontend/src/JS/ToDoList.js
@@ -11,11 +11,12 @@ export class ToDoList {
 
     }
     async getToDos(){
+        this.listEl.innerHTML="";
         let response = await fetch(this.url);
         let data = await response.json();
         for(let i = 0; i < data.length; i++){
             new ToDo(data[i], this.listEl);
-    }
+        }
 
-}
+    }
 }
